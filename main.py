51fcb02bd8b3d0f445f0eb184fe0119384808e03
Savion Ventura <savionventura@outlook.com>
2026-06-28 01:51:34 -0400

    Built Version 1 of the console trading journal

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-trade = {
-    "date": "2026-06-26",
-    "instrument": "GBP/USD",
-    "direction": "Long",
-    "entry": 1.2700,
-    "stop_loss": 1.2650,
-    "take_profit": 1.2800,
-    "result": "Win",
-    "notes": "First test trade in my AI Trading Journal."
-}
 
-print("Trade added successfully!")
-print(trade)
 
+
+trades = []
+
+valid_directions = ("long", "short") 
+
+while True:
+      print("\nAI Trading Journal")
+      print("1. Add Trade")
+      print("2.View Trades")
+      print("3. Quit")
+
+      choice = input("Choose an option: ")
+
+      if choice == "1":
+            symbol = input("Enter symbol: ") 
+            direction = input("Long or short: ").lower()
+
+            if direction not in valid_directions:
+                  print("Invalid direction")
+                  continue
+            
+            entry = float(input("Entry price: "))
+            exit_price = float(input("Exit price: "))
+
+            if direction == "long":
+                  pnl = exit_price - entry
+            else:
+                  pnl = entry - exit_price
+
+            if pnl > 0:
+                result = "Win"
+            elif pnl <0:
+                result = "Loss"
+            else:
+                result = "Break-even"
+            trade = [symbol, direction, entry, exit_price, pnl, result]
+            trades.append(trade)
+
+            print("Trade added.")
+      elif choice == "2":
+            if len(trades) == 0:
+                  print("No trades yet.")
+            else:
+                  for i in range(len(trades)):
+                        trade = trades[i]
+                        print(f"\nTrade #{i + 1}")
+                        print(f"Symbol: {trade[0]}")
+                        print(f"Direction: {trade[1]}")
+                        print(f"Entry: {trade[2]}")
+                        print(f"Exit: {trade[3]}")
+                        print(f"P/L: {trade[4]}")
+                        print(f"Result: {trade[5]}")
+      elif choice == "3":
+            print("Goodbye.")
+            break
+      else:
+            print("Invalid choice.")
